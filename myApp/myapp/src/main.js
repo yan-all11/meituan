8c591000c6845bc3a18297836f65a3b955e43db5
yanyudian <3555464376@qq.com>
2022-08-15 16:06:47 +0800

    文件路径修改

--- myApp/myapp/src/main.js
+++ myApp/myapp/src/main.js
@@ -5,7 +5,6 @@ import router from './router'
 import store from './store'
 import '@/assets/css/reset.css'
 import '@/assets/js/rem.js'
-
 import Vant from 'vant'
 import 'vant/lib/index.css'
 createApp(App).use(store).use(router).use(Vant).mount('#app')
--- myApp/myapp/vue.config.js
+++ myApp/myapp/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
+  // publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  // outputDir: 'dist',//构建输出目录
+  // assetsDir: 'assets',//静态资源目录(js,css,img,fonts)
+  // lintOnSave: false,//是否开启eslint保存检测,有效值: true || false || 'error'
+  // devServer: {
+  //   open: true,//是否自动弹出
+  //   host: 'localhost',
+  //   port: 8081,//端口
+  //   https: false,
+  //   hotOnly: false,//热更新
+  // }
 })
